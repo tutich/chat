b4253e0807150f2d0919082a9deddbc85044213a
tutich <pkoinange02@gmail.com>
2023-11-20 16:03:03 +0300

    react images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import avatar from './images/avatar.jpg';
 
 function App() {
   return (
     <div className="App">
+      <img src={avatar}/>
       Samuel Green
       Available to work
-     
+      That sounds great. i'd be happy with that.
+      Could you send over some pictures of your dog, please?
+      Here are some pictures,she's a happy girl.
+      Can you make it?
+      She looks so happy!The time we discussed works.How long shall I take her out for?
+      Simple booking
+      Stay in touch with our dog walker's through the chat interfacee. This makes it easy  to discuss arrangements
+      and make bookings. Once the walk has been completed you can rate your worker and book again all through 
+      the chat.
     </div>
   );
 }
