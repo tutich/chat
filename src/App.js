ab5b9d054e65a9a1a0d3d5ca39dccbb05b84196d
tutich <pkoinange02@gmail.com>
2023-11-20 16:25:10 +0300

    react jsx

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import avatar from './images/avatar.jpg';
+import dog1mg from './images/dog-image-1.jpg';
+import dog2mg from './images/dog-image-2.jpg';
+import dog3mg from './images/dog-image-3.jpg';
 
 function App() {
   return (
     <div className="App">
       <img src={avatar}/>
-      Samuel Green
-      Available to work
-      That sounds great. i'd be happy with that.
-      Could you send over some pictures of your dog, please?
-      Here are some pictures,she's a happy girl.
-      Can you make it?
-      She looks so happy!The time we discussed works.How long shall I take her out for?
-      Simple booking
-      Stay in touch with our dog walker's through the chat interfacee. This makes it easy  to discuss arrangements
+      <h2>Samuel Green</h2>
+      <h6>Available to walk</h6>
+      <p>That sounds great. i'd be happy with that.</p>
+      <p>Could you send over some pictures of your dog, please?</p>
+      <img src={dog1mg}/>
+      <img src={dog2mg}/>
+      <img src={dog3mg}/>
+      <p>Here are some pictures,she's a happy girl.</p>
+      <p>Can you make it?</p>
+      <p>She looks so happy!The time we discussed works.How long shall I take her out for?</p>
+      <h2>Simple booking</h2>
+      <p>Stay in touch with our dog walker's through the chat interfacee. This makes it easy  to discuss arrangements
       and make bookings. Once the walk has been completed you can rate your worker and book again all through 
-      the chat.
+      the chat.</p>
     </div>
   );
 }
